fc9e875188eb30a3a4b720ea66db192a482bb983
cviljean <vchedeline@gmail.com>
2022-03-15 23:28:54 -0400

    adds reset button markup and styling

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 console.log("We Are LIVE")
 /*----- constants -----*/
+// X & O - players
+// winning combinations
 
 /*----- app's state (variables) -----*/
 
+// where the users click (where to put an x or o) - make sure a square is not taken first
+// whose turn it is
+// when we have a winner or it there is a tie - if game is ongoing
+// Bonus - if the player wants to quit
+// Bonus - score for games won
+// Bonus - timr for each player
+
 /*----- cached element references -----*/
+
+// the game squares
+// reset button
 
 /*----- event listeners -----*/
 

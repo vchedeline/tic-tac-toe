5eaf87e698577ca310ff1878d846a21bcaa535d4
cviljean <vchedeline@gmail.com>
2022-03-16 00:12:33 -0400

    adds title and message container

--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
 console.log("We Are LIVE")
 /*----- constants -----*/
 // X & O - players
-// winning combinations
+const PLAYERS = {
+  '1': 'X',
+  '-1':'O',
+}
 
+// winning combinations
+// 012
+// 345
+// 678
+
+const COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+]
 /*----- app's state (variables) -----*/
 
 // where the users click (where to put an x or o) - make sure a square is not taken first
+let board; // stores board positions & what they hold
 // whose turn it is
+let turn; // eitehr X or O
 // when we have a winner or it there is a tie - if game is ongoing
+let winner; // can be X/O, tie, null
 // Bonus - if the player wants to quit
 // Bonus - score for games won
 // Bonus - timr for each player
